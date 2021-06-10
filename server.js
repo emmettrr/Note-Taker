8dab20fd79c96f474989acf9f3cb778f224d6932
Emmett Rushin <emmettrushin@gmail.com>
2021-06-10 14:03:22 -0400

    added more to server.js

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended:true })); 
+app.use(express.json()); 
+
+
+require("./Develop/routes/apiRoutes.js")(app); 
+require("./Develop/routes/htmlRoutes.js")(app); 
+
+
+app.listen(port, () => {
+    console.log(`App listening on port: ${port}`); 
+}); 
