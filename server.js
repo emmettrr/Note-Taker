ad1aabd600ae8ed76146a4a05ecbc1d807c5472d
Emmett Rushin <emmettrushin@gmail.com>
2021-06-10 18:11:08 -0400

    fixed formatting

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended:true })); 
-app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-require("./routes/apiRoutes.js")(app); 
-require("./routes/htmlRoutes.js")(app); 
-
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
 app.listen(port, () => {
-    console.log(`App listening on port: ${port}`); 
-}); 
+  console.log(`App listening on port: ${PORT}`);
+});
