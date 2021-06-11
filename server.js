8fedb4b2f5d5682e8f552ec1f6d1f003bcb1f7a4
Emmett Rushin <emmettrushin@gmail.com>
2021-06-11 15:11:41 -0400

    fixed line 8 in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static(__dirname + "/public"));
 
 require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
