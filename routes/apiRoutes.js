da0c6d4d7343a0e07fef2e1da803e891236dcf8f
Emmett Rushin <emmettrushin@gmail.com>
2021-06-11 13:33:27 -0400

    added more to apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,32 @@
 const fs = require("fs");
-const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+const data = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
 
 modules.export = (app) => {
   app.get("/api/notes", (res, req) => {
     res.json(data);
   });
-  app.get("/api/notes/:id", (res,req) => {
-      res.json(data[Number(req.params.id)])
-  })
+  app.get("/api/notes/:id", (res, req) => {
+    res.json(data[Number(req.params.id)]);
+  });
 
-  
+  app.post("/api/notes", (res, req) => {
+    let note = req.body;
+    let uniqueID = data.length;
+    console.log(uniqueID);
+    note.id = uniqueID;
+    data.push(note)
+
+    fs.writeFileSync("../db/db.json", JSON.stringify(data), (err) => {
+        if (err) console.log(err) 
+    })
+
+    res.json(data)
+  });
+
+
+
+
+
+
+
 };
