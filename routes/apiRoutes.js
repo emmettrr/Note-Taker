61f098a1b8bc674fc338c0aa0dbd26ce9dee5e9a
Emmett Rushin <emmettrushin@gmail.com>
2021-06-11 15:15:30 -0400

    fixed typos

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = (app) => {
-  app.get("/api/notes", (res, req) => {
+  app.get("/api/notes", (req, res) => {
     res.json(data);
   });
-  app.get("/api/notes/:id", (res, req) => {
+  app.get("/api/notes/:id", (req, res) => {
     res.json(data[Number(req.params.id)]);
   });
 
-  app.post("/api/notes", (res, req) => {
+  app.post("/api/notes", (req, res) => {
     let note = req.body;
     let uniqueID = data.length;
     console.log(uniqueID);
